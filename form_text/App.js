ee443f9adfb93dfa4cbf6aab5b0cf78027a32435
Victor Augusto de Almeida Silva <victoraugustoas41@gmail.com>
2022-09-16 17:11:38 -0300

    Utilizando css no projeto de React-Native

--- form_text/App.js
+++ form_text/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StatusBar, Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function AppendMode() {
+export default function App() {
+  const [ num1, setNum1 ] = useState(''); 
+  const [ num2, setNum2 ] = useState('');
+  const [ res, setRes ] = useState();
+
+  function somar() {
+    let n1 = Number(num1)
+    let n2 = Number(num2)
+    let soma = n1 + n2;
+    setRes(soma);
+  }
+
   return (
     <SafeAreaView style={style.container}>
       <StatusBar backgroundColor='#FF7E01' barStyle='dark-content' translucent={false}></StatusBar>
-      <View>
+      <View style={style.viewcontainer}>
         <Text style={style.header}>Caixa de Texto</Text>
-        <TextInput style={style.input} placeholder="Digite o Nome" placeholderTextColor='white' secureTextEntry={true}></TextInput>
-        <TouchableOpacity style={style.button}>
+        <TextInput style={style.input} placeholder="Digite o valor" placeholderTextColor='white' secureTextEntry={false} value={num1} onChangeText={setNum1} keyboardType='numeric'></TextInput>
+        <TextInput style={style.input} placeholder="Digite o valor" placeholderTextColor='white' secureTextEntry={false} value={num2} onChangeText={setNum2} keyboardType='numeric'></TextInput>
+        <TouchableOpacity style={style.button} onPress={somar}>
           <Text style={style.textInput}>Enviar</Text>
         </TouchableOpacity>
+        <Text style={style.resultado}>{res}</Text>
       </View>
     </SafeAreaView>
   )
@@ -25,6 +38,7 @@ const style = StyleSheet.create({
   header: {
     fontSize: 25,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   input: {
     backgroundColor: '#30B6FF',
@@ -40,9 +54,21 @@ const style = StyleSheet.create({
   },
   button: {
     marginTop: 15,
-    backgroundColor:'#FF0034',
+    backgroundColor: '#FF0034',
     height: 40,
     borderRadius: 8,
     textAlign: 'center',
+  },
+  resultado: {
+    backgroundColor: 'green',
+    height: 40,
+    borderRadius: 8,
+    marginTop: 15,
+    textAlign: "center" ,
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  viewcontainer: {
+    width: '90%',
   },
 })
